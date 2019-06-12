830053cb3d35b55a19a8681b1775f0a588545f49
Michael Liu <liuke_lk@163.com>
2019-06-12 10:53:35 +0100

    change led color.

--- sdk/interface/led.py
+++ sdk/interface/led.py
@@ -20,18 +20,18 @@ class Led(object):
 
     def led_open(self, directive):
         logging.info('[DuerOS]开灯.........')
-        self.dev.set_pixel_rgb(1, 0xFF0000)  # Red
-        self.dev.set_pixel_rgb(2, 0xFFFFFF)  # White
-        self.dev.set_pixel_rgb(3, 0x00FF00)  # Green
-        self.dev.set_pixel_rgb(4, 0xFF0000)  # Red
-        self.dev.set_pixel_rgb(5, 0xFFFFFF)  # White
-        self.dev.set_pixel_rgb(6, 0x00FF00)  # Green
-        self.dev.set_pixel_rgb(7, 0xFF0000)  # Red
-        self.dev.set_pixel_rgb(8, 0xFFFFFF)  # White
-        self.dev.set_pixel_rgb(9, 0x00FF00)  # Green
-        self.dev.set_pixel_rgb(10, 0xFF0000)  # Red
-        self.dev.set_pixel_rgb(11, 0xFFFFFF)  # White
-        self.dev.set_pixel_rgb(0, 0x00FF00)  # Green
+        self.dev.set_pixel_rgb(0, 0xFF0000)  # 红色
+        self.dev.set_pixel_rgb(1, 0xFF7F00)  # 橙色
+        self.dev.set_pixel_rgb(2, 0xFFFF00)  # 黄色
+        self.dev.set_pixel_rgb(3, 0x00FF00)  # 绿色
+        self.dev.set_pixel_rgb(4, 0x00FFFF)  # 青色
+        self.dev.set_pixel_rgb(5, 0x0000FF)  # 蓝色
+        self.dev.set_pixel_rgb(6, 0x8B00FF)  # 紫色
+        self.dev.set_pixel_rgb(7, 0xFF0000)  #
+        self.dev.set_pixel_rgb(8, 0xFF7F00)  #
+        self.dev.set_pixel_rgb(9, 0xFFFF00)  #
+        self.dev.set_pixel_rgb(10, 0x00FF00)  #
+        self.dev.set_pixel_rgb(11, 0x00FFFF)  #
         self.dev.show()
 
     def led_close(self, directive):
